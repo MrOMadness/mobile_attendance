2e342c8611aa08e401f269c110213efec6102582
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-04-14 21:09:42 +0700

    Create styles.dart

---
+++ lib/styles.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const String poppinsFont = 'poppins';
+  static const int lightBlue = 0xffD8E3E7;
+  static const int grey = 0xff665555;
+
+  static const TextStyle black_16 = TextStyle(
+    fontFamily: poppinsFont,
+    fontSize: 16,
+  );
+
+  static const TextStyle grey_13 = TextStyle(
+    fontFamily: poppinsFont,
+    fontSize: 13,
+    color: Color(grey),
+  );
+}
