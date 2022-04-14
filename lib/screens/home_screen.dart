ec8ffd967d1072baf126374a6c81f0ec2e7daf1a
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-04-14 22:20:37 +0700

    Change name from box_template to rounded_box_template

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_attendance/screens/change_location_screen.dart';
-import 'package:mobile_attendance/templates/box_template.dart';
+import 'package:mobile_attendance/templates/rounded_box_template.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
