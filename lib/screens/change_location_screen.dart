757e4e4da0ccec40a4fe3b1c19fb1592c7e28474
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-04-14 21:13:47 +0700

    Create change_location_screen.dart

---
+++ lib/screens/change_location_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChangeLocationScreen extends StatefulWidget {
+  const ChangeLocationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChangeLocationScreen> createState() => _ChangeLocationScreenState();
+}
+
+class _ChangeLocationScreenState extends State<ChangeLocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
