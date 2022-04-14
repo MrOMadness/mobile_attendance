4c85c5d00e54ea42e1a7265ecfa651c69e2ebf0a
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-04-14 18:03:10 +0700

    Added navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_attendance/nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: NavBar(),
+      home: const NavBar(),
     );
   }
 }
---
+++ lib/nav_bar.dart
@@ -0,0 +1,54 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_attendance/screens/add_screen.dart';
+import 'package:mobile_attendance/screens/history_screen.dart';
+import 'package:mobile_attendance/screens/home_screen.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    HistoryScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddScreen()),
+          );
+        },
+        child: const Icon(
+          Icons.qr_code_scanner_outlined,
+          color: Colors.black,
+        ),
+        // backgroundColor: const Color(Styles.lightBlue),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: iconList,
+        activeIndex: _selectedIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.softEdge,
+        leftCornerRadius: 20, //32
+        rightCornerRadius: 20,
+        onTap: (index) => setState(() => _selectedIndex = index),
+      ),
+    );
+  }
+}
+
+const iconList = [Icons.home, Icons.account_circle];
