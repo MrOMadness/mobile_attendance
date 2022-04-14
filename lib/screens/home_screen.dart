9269268504cda32de774f53fb36e6eb88152c21b
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-04-15 01:21:23 +0700

    get coords data from firestore

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_attendance/screens/change_location_screen.dart';
 import 'package:mobile_attendance/templates/rounded_box_template.dart';
@@ -10,22 +11,42 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final Stream<DocumentSnapshot> _constantsStream =
+      FirebaseFirestore.instance.collection('constants').doc('HQ').snapshots();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-          padding: const EdgeInsets.fromLTRB(12, 48, 12, 12),
-          children: [
-            GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ChangeHQLocationScreen(132, -12)),
-                  );
-                },
-                child: const RoundedBoxTemplate('Current Location', ''))
-          ]),
-    );
+    return StreamBuilder<DocumentSnapshot>(
+        stream: _constantsStream,
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading");
+          }
+
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+
+          return Scaffold(
+            body: ListView(
+                padding: const EdgeInsets.fromLTRB(12, 48, 12, 12),
+                children: [
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ChangeHQLocationScreen(
+                                  data['latitude'], data['longitude'])),
+                        );
+                      },
+                      child: const RoundedBoxTemplate('Current Location', ''))
+                ]),
+          );
+        });
   }
 }
