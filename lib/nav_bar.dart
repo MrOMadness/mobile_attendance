6575d3657cf98c530689005a0d1f885b8076c85d
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-04-15 14:55:08 +0700

    fix and clean code

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -1,6 +1,7 @@
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_attendance/screens/add_screen.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:mobile_attendance/screens/history_screen.dart';
 import 'package:mobile_attendance/screens/home_screen.dart';
 
@@ -13,42 +14,70 @@ class NavBar extends StatefulWidget {
 
 class _NavBarState extends State<NavBar> {
   int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    HomeScreen(),
-    HistoryScreen(),
-  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddScreen()),
+    // Stream of DocumentSnapshot from firebase
+    final Stream<DocumentSnapshot> _constantsStream = FirebaseFirestore.instance
+        .collection('constants')
+        .doc('HQ')
+        .snapshots();
+
+    return StreamBuilder<DocumentSnapshot>(
+        stream: _constantsStream,
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          // error screen
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+          // loading screen
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading");
+          }
+
+          Map<String, dynamic> streamData =
+              snapshot.data!.data() as Map<String, dynamic>;
+
+          return Scaffold(
+            body: Center(
+              child: [
+                HomeScreen(streamData),
+                const HistoryScreen(),
+              ].elementAt(_selectedIndex),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () async {
+                await checkPositionValidity();
+                print('done');
+              },
+              child: const Icon(
+                Icons.qr_code_scanner_outlined,
+                color: Colors.black,
+              ),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            bottomNavigationBar: AnimatedBottomNavigationBar(
+              // Library from AnimatedBottomNavigationBar
+              icons: iconList,
+              activeIndex: _selectedIndex,
+              gapLocation: GapLocation.center,
+              notchSmoothness: NotchSmoothness.softEdge,
+              leftCornerRadius: 20,
+              rightCornerRadius: 20,
+              onTap: (index) => setState(() => _selectedIndex = index),
+            ),
           );
-        },
-        child: const Icon(
-          Icons.qr_code_scanner_outlined,
-          color: Colors.black,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        // Library from AnimatedBottomNavigationBar
-        icons: iconList,
-        activeIndex: _selectedIndex,
-        gapLocation: GapLocation.center,
-        notchSmoothness: NotchSmoothness.softEdge,
-        leftCornerRadius: 20, //32
-        rightCornerRadius: 20,
-        onTap: (index) => setState(() => _selectedIndex = index),
-      ),
-    );
+        });
   }
 }
 
 const iconList = [Icons.home, Icons.account_circle]; // Icons for bottom nav bar
+
+Future<void> checkPositionValidity() async {
+  Position userPosition = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.best);
+
+  print('');
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,98 +1,75 @@
 import 'dart:async';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobile_attendance/screens/change_location_screen.dart';
 import 'package:mobile_attendance/templates/rounded_box_template.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final Map<String, dynamic> streamData;
+  const HomeScreen(this.streamData, {Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // Stream of DocumentSnapshot from firebase
-  final Stream<DocumentSnapshot> _constantsStream =
-      FirebaseFirestore.instance.collection('constants').doc('HQ').snapshots();
-
-  // ignore: prefer_final_fields
-  Completer<GoogleMapController> _controller = Completer();
+  final Completer<GoogleMapController> _controller = Completer();
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<DocumentSnapshot>(
-        stream: _constantsStream,
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          // error screen
-          if (snapshot.hasError) {
-            return const Text('Something went wrong');
-          }
-          // loading screen
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text("Loading");
-          }
+    CameraPosition hqPosition = CameraPosition(
+      target:
+          LatLng(widget.streamData['latitude'], widget.streamData['longitude']),
+      zoom: 18, // set zoom level
+    );
 
-          Map<String, dynamic> data =
-              snapshot.data!.data() as Map<String, dynamic>;
+    Future<void> updateLocation() async {
+      final GoogleMapController controller = await _controller.future;
+      controller.animateCamera(CameraUpdate.newCameraPosition(hqPosition));
+    }
 
-          CameraPosition hqPosition = CameraPosition(
-            target: LatLng(data['latitude'], data['longitude']),
-            zoom: 18, // set zoom level
-          );
-
-          Future<void> updateLocation() async {
-            final GoogleMapController controller = await _controller.future;
-            controller
-                .animateCamera(CameraUpdate.newCameraPosition(hqPosition));
-          }
-
-          return Scaffold(
-            body: ListView(
-                padding: const EdgeInsets.fromLTRB(12, 48, 12, 12),
-                children: [
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ChangeHQLocationScreen(
-                                  data['latitude'], data['longitude'])),
-                        );
-                      },
-                      child:
-                          const RoundedBoxTemplate('Change HQ Location', '')),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(5, 10, 5, 0),
-                    height: 250,
-                    child: Stack(children: [
-                      FutureBuilder(
-                          future: updateLocation(),
-                          builder: (context, snapshot) {
-                            return GoogleMap(
-                              initialCameraPosition: hqPosition,
-                              onMapCreated:
-                                  (GoogleMapController controller) async {
-                                _controller.complete(controller);
-                              },
-                            );
-                          }),
-                      const Center(
-                        child: Positioned(
-                          child: Icon(
-                            Icons.person_pin_circle,
-                            size: 40,
-                            color: Colors.lightBlue,
-                          ),
-                        ),
-                      ),
-                    ]),
-                  )
-                ]),
-          );
-        });
+    return Scaffold(
+      body: ListView(
+          padding: const EdgeInsets.fromLTRB(12, 48, 12, 12),
+          children: [
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ChangeHQLocationScreen(
+                            widget.streamData['latitude'],
+                            widget.streamData['longitude'])),
+                  );
+                },
+                child: const RoundedBoxTemplate('Change HQ Location', '')),
+            Container(
+              margin: const EdgeInsets.fromLTRB(5, 10, 5, 0),
+              height: 250,
+              child: Stack(children: [
+                FutureBuilder(
+                    future: updateLocation(),
+                    builder: (context, snapshot) {
+                      return GoogleMap(
+                        initialCameraPosition: hqPosition,
+                        onMapCreated: (GoogleMapController controller) async {
+                          _controller.complete(controller);
+                        },
+                      );
+                    }),
+                const Center(
+                  child: Positioned(
+                    child: Icon(
+                      Icons.person_pin_circle,
+                      size: 40,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                ),
+              ]),
+            )
+          ]),
+    );
   }
 }
