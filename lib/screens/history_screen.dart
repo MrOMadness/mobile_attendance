db54324e61a7dd7c1ea9ab593c1db4f6c245cf31
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-04-15 19:55:58 +0700

    Add today's attendance

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -30,21 +30,36 @@ class _HistoryScreenState extends State<HistoryScreen> {
             }
             return ListView(
               children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                // Map<String, dynamic> data =
-                //     document.data()! as Map<String, dynamic>;
-                return ListTile(
-                  title: Text(document.id),
-                  subtitle:
-                      Text("Today's attendance: ${checkLoginToday(document)}"),
-                );
+                return FutureBuilder(
+                    future: checkAttendanceToday(document),
+                    builder: (context, snapshot) {
+                      print(snapshot.data);
+                      return ListTile(
+                        title: Text(document.id),
+                        subtitle: Text("Today's attendance: ${snapshot.data}"),
+                      );
+                    });
               }).toList(),
             );
           }),
     );
   }
 
-  String checkLoginToday(document) {
-    print(document);
-    return 'present';
+  Future<String> checkAttendanceToday(DocumentSnapshot document) async {
+    Timestamp lastValidAttendance = await document.get('last_valid_attendance');
+
+    if (calculateDifference(lastValidAttendance.toDate()) == 0) {
+      return 'present';
+    }
+    return 'absent';
+  }
+
+  // https://stackoverflow.com/questions/54391477/check-if-datetime-variable-is-today-tomorrow-or-yesterday
+  // Returns the difference (in full days) between the provided date and today.
+  int calculateDifference(DateTime date) {
+    DateTime now = DateTime.now();
+    return DateTime(date.year, date.month, date.day)
+        .difference(DateTime(now.year, now.month, now.day))
+        .inDays;
   }
 }
